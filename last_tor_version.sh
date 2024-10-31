a6802f1dc2c3b7292018350f8c43a021b6c485b1
Seth For Privacy <40500387+sethforprivacy@users.noreply.github.com>
2024-10-31 14:49:04 -0400

    Bump Tor to v0.4.8.13 and Torsocks to v2.4.0

--- last_tor_version.sh
+++ last_tor_version.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git ls-remote --tags https://git.torproject.org/tor.git | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -1
+git ls-remote --tags https://gitlab.torproject.org/tpo/core/tor.git/ | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -1
--- last_torsocks_version.sh
+++ last_torsocks_version.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git ls-remote --tags https://git.torproject.org/torsocks.git | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -1
+git ls-remote --tags https://gitlab.torproject.org/tpo/core/torsocks.git/ | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -1
