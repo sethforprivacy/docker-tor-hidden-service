0f5b7babeedaa44256c99471c9bf857c43c834b1
Seth For Privacy <40500387+sethforprivacy@users.noreply.github.com>
2024-11-01 08:52:05 -0400

    Broad Dockerfile optimizations and cleaned up unnecessary files

--- last_tor_version.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git ls-remote --tags https://gitlab.torproject.org/tpo/core/tor.git/ | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -1
--- last_torsocks_version.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git ls-remote --tags https://gitlab.torproject.org/tpo/core/torsocks.git/ | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -1
